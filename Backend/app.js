3e2f092433a3b420030af9ef08dc127f9ca376b9
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-12 17:13:25 +0530

    added built in middleware of express js

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,10 @@ const app = express();
 const connectToDB = require("./config/db");
 const userRoutes = require("./route/userRoutes");
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 connectToDB();
 app.use("/", userRoutes);
 
