0554522762b18a4e7ab282c3615605072c6f3562
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-18 22:58:09 +0530

    userList component added

---
+++ frontend/src/Components/UserList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+export const UserList = () => {
+  const [userData, setUserData] = useState(null);
+
+  return (
+    <>
+      <h1>this is user list</h1>
+    </>
+  );
+};
